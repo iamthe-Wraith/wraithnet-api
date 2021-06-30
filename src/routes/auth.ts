c54beccf1ba9133c36792826b2bc9a9085c99875
iamthe-Wraith <allhailthewraith@protonmail.com>
2021-06-29 21:03:57 -0400

    added new /auth/verify-token endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import { AuthController } from '../controllers/auth';
+import { authMiddleware } from '../middleware/auth';
 
 const router = Router();
 
 router.route('/')
   .post(AuthController.authenticate);
+
+router.route('/verify-token')
+  .post(authMiddleware, AuthController.verifyToken);
 
 export const authRouter = router;
