18dd084953e210028da4044524a87f8fee6293ce
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-01-08 10:44:39 -0500

    adds search to images modal

--- src/controllers/image.ts
+++ src/controllers/image.ts
@@ -1,13 +1,34 @@
+/* eslint-disable radix */
 import { RequestHandler } from 'express';
 
 import { Response } from '../utils/response';
 import { IRequest } from '../types/request';
 import { ImageService } from '../services/image';
+import CustomError from '../utils/custom-error';
+import { ERROR } from '../constants';
 
 export class ImageController {
     static getImages:RequestHandler = async (req:IRequest, res) => {
+        const { page, pageSize, fileName } = (req.query as { page: string, pageSize: string, fileName: string });
         try {
-            const images = await ImageService.getImages(req);
+            let _page = 0;
+            if (page) {
+                _page = parseInt(page);
+                if (isNaN(_page)) throw new CustomError('invalid page found', ERROR.INVALID_ARG);
+            }
+
+            let _pageSize = 50;
+            if (pageSize) {
+                _pageSize = parseInt(pageSize);
+                if (isNaN(_pageSize)) throw new CustomError('invalid pageSize found', ERROR.INVALID_ARG);
+            }
+
+            const images = await ImageService.getImages({
+                requestor: req.requestor,
+                page: _page,
+                pageSize: _pageSize,
+                fileName,
+            });
             Response.send({ ...images, results: images.results.map(image => ImageService.getSharableImage(image)) }, req, res);
         } catch (err: any) {
             Response.error(err, req, res);
--- src/services/image.ts
+++ src/services/image.ts
@@ -8,35 +8,36 @@ import {
 import { ERROR } from '../constants';
 import { IRequest } from '../types/request';
 import CustomError, { asCustomError } from '../utils/custom-error';
+import { IUser } from '../models/user';
 
 const S3 = require('aws-sdk/clients/s3');
 
 dayjs.extend(utc);
 
-export class ImageService {
-    static async getImages(req: IRequest): Promise<IImages> {
-        const { page, pageSize } = (req.query as { page: string, pageSize: string });
+export interface IGetImageArgs {
+    requestor: IUser;
+    page?: number,
+    pageSize?: number,
+    fileName?: string,
+}
 
+export class ImageService {
+    static async getImages({
+        requestor,
+        page,
+        pageSize,
+        fileName,
+    }: IGetImageArgs): Promise<IImages> {
         const query: any = {
-            $and: [{ owner: req.requestor.id }],
+            $and: [{ owner: requestor.id }],
         };
 
-        let _page = 0;
-        if (page) {
-            _page = parseInt(page);
-            if (isNaN(_page)) throw new CustomError('invalid page found', ERROR.INVALID_ARG);
-        }
-
-        let _pageSize = 50;
-        if (pageSize) {
-            _pageSize = parseInt(pageSize);
-            if (isNaN(_pageSize)) throw new CustomError('invalid pageSize found', ERROR.INVALID_ARG);
-        }
+        if (!!fileName) query.$and.push({ fileName: { $regex: fileName, $options: 'i' } });
 
         const results = await Image
             .find(query)
-            .skip(_page * _pageSize)
-            .limit(_pageSize)
+            .skip(page * pageSize)
+            .limit(pageSize)
             .sort({ fileName: 'asc' })
             .exec();
 
